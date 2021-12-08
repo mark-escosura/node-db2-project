aebf26df9e4ecbac0f37b34281a0b747196a7644
mark-escosura <escosuramarkse@gmail.com>
2021-12-07 20:20:51 -0800

    Commit: Routing and Middleware Done

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,48 @@
 // DO YOUR MAGIC
+const express = require("express");
+
+const Car = require("./cars-model.js");
+
+const router = express.Router();
+
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+} = require("./cars-middleware");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const cars = await Car.getAll();
+    res.json(cars);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", checkCarId, async (req, res, next) => {
+  try {
+    res.json(req.car);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/',checkCarPayload, checkVinNumberValid, checkVinNumberUnique, async (req, res, next)=>{
+    try{
+        const car = await Car.create(req.body)
+        res.json(car)
+    }catch (error){
+        next(error)
+    }
+})
+
+router.use((err, req, res, next)=>{ // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,13 @@
 const express = require("express")
-
+const carsRouter = require('./cars/cars-router')
 const server = express()
 
-// DO YOUR MAGIC
+server.use(express.json())
+server.use('/api/cars', carsRouter)
+
+
+server.get('/', (req, res)=>{
+    res.status(200).json({message: "it's working"})
+})
 
 module.exports = server
